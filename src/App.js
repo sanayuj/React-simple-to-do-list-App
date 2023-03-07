68ab4ad3abd886447faa162e99dd6907090351af
sanayuj <sanayuj2255@gmail.com>
2023-03-07 11:20:59 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
-import "./App.css";
-import { useState } from "react";
+import React,{useEffect, useState} from 'react';
+import './App.css';
 
 function App() {
-  const [toDos, setToDos] = useState([]);
-  const [toDo, setToDo] = useState("");
+
+
+  const [date,setDate]=useState('')
+  const [toDos,setToDos]=useState([])
+  const[toDo,setToDo]=useState('')
+  const today = new Date();
+
+  useEffect(() => {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayName = daysOfWeek[today.getDay()];
+  
+    setDate(dayName)
+
+  }, [])
+  
+  const handleToDo=()=>{
+    setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])
+    setToDo("")
+  }
+ 
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,68 +29,46 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>It's Wednesday </h2>
+        <h2>Whoop, it's {date} 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input
-          value={toDo}
-          onChange={(e) => {
-            setToDo(e.target.value);
-          }}
-          type="text"
-          placeholder="🖊️ Add item..."
-        />
-        <i
-          onClick={() =>
-            setToDos([...toDos,{ id: Date.now, text: toDo, status: false }])
-          }
-          className="fas fa-plus"
-        ></i>
+        <input type="text" value={toDo} onChange={(e)=>setToDo(e.target.value)} placeholder="🖊️ Add item..." />
+        
+        <i onClick={handleToDo} className="fas fa-plus"></i>
       </div>
-
       <div className="todos">
-        {toDos.map((obj) => {
-          return (
-            <div className="todo">
-              <div key={obj.id} className="left">
-                <input
-                  onChange={(e) => {
-                    console.log(e.target.checked);
-                    console.log(obj);
-                    setToDos(
-                      toDos.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.status = e.target.checked;
-                        }
-                        return obj2;
-                      })
-                    );
-                  }}
-                  value={obj.status}
-                  type="checkbox"
-                  name=""
-                  id=""
-                />
-                <p>{obj.text}</p>
-              </div>
-              <div className="right">
-                <i onClick={(e)=>{
-                  setToDos(toDos.filter((obj3)=>{
-                    if(obj3.id!==obj.id){
-                      return obj3
-                    }
-                  }))
-                }} className="fas fa-times"></i>
-              </div>
-            </div>
-          );
-        })}
-        {toDos.map((obj) => {
-          if (obj.status) {
-            return <h1>{obj.text}</h1>;
-          }
-          return null;
-        })}
+        {
+          toDos.map((obj)=>{ 
+            return(  
+        <div key={obj.id} className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+           
+              console.log(obj)
+              console.log(e.target.checked)
+             
+              setToDos(toDos.filter((obj2)=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2;
+              }))
+            }}  type="checkbox" name="" id="" />
+            <p className={obj.status?'done':''}>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i onClick={()=>{setToDos(
+              toDos.filter((obj3)=>{
+                if(obj3.id!==obj.id){
+                  return obj3
+                }
+              })
+            )}} className="fas fa-times"></i>
+          </div>
+        </div>
+            )
+         })
+        }
       </div>
     </div>
   );
